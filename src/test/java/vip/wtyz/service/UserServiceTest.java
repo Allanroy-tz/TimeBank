106864756f20262da55bfca9481716d0702c06fb
Allanroy <oncwnuB8MvWUpjNaWTTNXFDNd0s8@git.weixin.qq.com>
2024-05-17 21:34:00 +0800

    {}

---
+++ src/test/java/vip/wtyz/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package vip.wtyz.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void insertUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void userLogin() {
+
+    }
+}
